aded255bfd2119a3337ec0f86985827b95148c54
Adam Mornes <Adam.Mornes@xcentium.com>
2019-09-06 12:56:29 -0500

    Gulp task cleanup

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,32 +3,58 @@
 import gulp from 'gulp';
 import webpack from 'webpack';
 import path from 'path';
-import serve from 'browser-sync';
+import browserSync from 'browser-sync';
 import del from 'del';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import colorsSupported from 'supports-color';
 import historyApiFallback from 'connect-history-api-fallback';
 import log from 'fancy-log';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
 import PluginError from 'plugin-error';
+import paths from './config/env/paths';
+import outputNames from './config/env/output';
 
-const paths = {
-    src: path.join(__dirname, 'src'),
-    demo: path.join(__dirname, 'demo'),
-    dist: path.join(__dirname, 'dist')
+const setupCopyWebpackPlugin = (dir, isProd = false) => {
+    const ignore = isProd ? ['demo/**/*'] : [];
+    let patterns = [
+        { 
+            from: `${paths.src}/assets/fonts`, 
+            to: `${dir}/fonts` 
+        },
+        { 
+            from: `${paths.src}/assets/images`, 
+            to: `${dir}/images`, 
+            ignore
+        }
+    ]
+    const plugin = [
+        new CopyWebpackPlugin([ ...patterns ])
+    ]
+    return plugin;
 }
 
 /**
- * Description: Deletes dist folder and contents
+ * Description: Deletes dist/prod folder and contents
  *
- * Usage: gulp clean
+ * Usage: gulp clean-prod | gulp clean-demo
  */
 
-export const clean = (cb) => del([paths.dist])
-    .then(function (paths) {
-        log.info('[clean]', paths);
+const clean = cb => del(paths.prod)
+    .then(dirs => {
+        log.info('[clean]', dirs);
         cb();
     });
+
+export const cleanProdTask = gulp.task('clean-prod', gulp.series(clean));
+
+const cleanDist = cb => del([paths.dist])
+    .then(dirs => {
+        log.info('[clean]', dirs);
+        cb();
+    });
+
+export const cleanDistTask = gulp.task('clean-demo', gulp.series(cleanDist));
 
 /**
  * Description: Starts a development server.
@@ -39,11 +65,11 @@ const startDevServer = () => {
     const config = require('./config/webpack.dev.config');
     const compiler = webpack(config);
 
-    serve({
+    browserSync({
         port: process.env.PORT || 3000,
         open: false,
         server: {
-            baseDir: 'src'
+            baseDir: paths.src
         },
         middleware: [
             historyApiFallback(),
@@ -73,30 +99,53 @@ const buildProd = (cb) => {
         paths.src
     );
 
+    config.plugins = config.plugins.concat(setupCopyWebpackPlugin(paths.prod, true));
+    config.output.publicPath = '/assets/';
+
     build(config, cb);
 }
 
 export const prod  = gulp.series(clean, buildProd);
 
 /**
- * Description: Build the Demo site out as minified assets
+ * Description: Serves dist directory built from gulp demo.
+ *
+ * Usage: gulp serve
+ */
+const startDemoServer = () => {
+    browserSync({
+        port: process.env.PORT || 3000,
+        open: false,
+        server: {
+            baseDir: paths.dist
+        }
+    });
+}
+
+export const serve = gulp.series(startDemoServer);
+
+/**
+ * Description: Build and serve the Demo site out as minified assets
  *
  * Usage: gulp demo
  */
 const buildDemo = (cb) => {
-    const config = setEntry(
+    let config = setEntry(
         require('./config/webpack.dist.config'),
         paths.demo
     );
 
+    config.output.path = paths.dist;
+    config.plugins = config.plugins.concat(setupCopyWebpackPlugin(`${paths.dist}/assets`, false));
+
     build(config, cb);
 }
 
-export const demo = gulp.series(clean, buildDemo);
+export const demo = gulp.series(cleanDist, buildDemo, startDemoServer);
 
 // Adds entry path to webpack config file
 function setEntry(config, entryPath) {
-    config.entry.main = path.join(entryPath, 'index.js');
+    config.entry[outputNames.scripts] = path.join(entryPath, 'index.js');
     return config;
 }
 
